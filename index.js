a3d11f1c51b84985a0546b55bb0b537badef58c6
GSvenny <gabrielswagner7@gmail.com>
2022-07-01 19:55:02 -0500

    added questions, adapting module's writefile code

--- index.js
+++ index.js
@@ -79,7 +79,11 @@ inquirer.prompt([
   
 ]).then((response) => {  
     renderLicenseBadge(license);
-    console.log(response);
+    //fs.writeFile('index.html', generatePage(name, github), err => {
+  //if (err) throw err;
+
+  //console.log('Portfolio complete! Check out index.html to see the output!');
+});
 }) 
 
 // }
